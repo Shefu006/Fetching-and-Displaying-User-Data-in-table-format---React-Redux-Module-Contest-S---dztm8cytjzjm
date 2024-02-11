46f79f52fb9f94636bc6cc375f1443cf2944af42
Shefali agarwal <103130093+Shefu006@users.noreply.github.com>
2024-02-11 17:46:40 +0530

    Update UserTable.js

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,5 +1,46 @@
 import React, { useState, useEffect } from "react";
 
-function UserTable() {}
+function UserTable() {import React, { useState, useEffect } from "react";
+
+function UserTable() {
+    const [userData, setUserData]=useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(data => {
+            const sortedUserData = data.sort((a, b) => a.id - b.id);
+            setUserData(sortedUserData);
+          })
+          .catch(error => console.error('Error fetching user data:', error));
+      }, []);
+      return(
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userData.map(user=>(
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.website}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+}
+
+export default UserTable;
+
+  
+}
 
 export default UserTable;
